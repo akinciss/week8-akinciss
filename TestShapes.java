a83f58302bc276128c4ceac8fb6ccad049fe5812
Simge Akıncı <148821014+akinciss@users.noreply.github.com>
2025-04-18 12:40:49 +0300

    TestShapes

---
+++ TestShapes.java
@@ -0,0 +1,11 @@
+public class TestShapes {
+    public static void main(String[] args){
+
+        Shape shape = new Shape();
+        shape.describe();
+
+        Rectangle r1 = new Rectangle(color: "blue ",sideA :4 )
+        Rectangle r2 = new Rectangle(color: "blue ",sideA :4 )
+        System.out.println();
+    }
+}
